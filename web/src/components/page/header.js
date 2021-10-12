e7f09e4cf1f22279c03cddcceff5efbdee5f39e0
Liwen Duan <hi@liwenduan.com>
2021-10-12 10:30:47 +0800

    add heroicon by tailwindcss

--- web/src/components/page/header.js
+++ web/src/components/page/header.js
@@ -1,12 +1,18 @@
 import React, {useState} from 'react'
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
-import Menu from "./static/menu.svg"
-import Close from "./static/close.svg"
+import {GlobeAltIcon, MenuIcon, XIcon } from '@heroicons/react/solid'
+
 
 const NavButton = ({LinkTo, children}) => {
     return (
         <Link to={LinkTo} className="flex w-full md:w-28 p-3 my-3 md:m-3 justify-center rounded-md md:shadow-md font-medium text-gray-700 hover:text-black focus:text-black hover:bg-gray-100 focus:bg-yellow-300">{children}</Link>
+    )
+}
+
+const LanguageSwitcher = () => {
+    return (
+        <GlobeAltIcon className="flex h-12 w-full md:w-auto p-3 my-3 md:m-3 justify-center rounded-full text-gray-700 hover:text-black focus:text-black hover:bg-gray-100 focus:bg-yellow-300 text-center align-center"/>
     )
 }
 
@@ -33,14 +39,15 @@ const Header = ({pageName='lwdSite'}) => {
             <div className="flex-grow"></div>
             <div className="flex items-center md:hidden p-2 ">
                 <button onClick={() => setMenuOpen(!isMenuOpen)} className={`p-3 rounded-lg shadow-md`}>
-                    {isMenuOpen ? <Close /> : <Menu />}
+                    {isMenuOpen ? <XIcon className="h-8 w-full md:w-auto" /> : <MenuIcon className="h-8 w-full md:w-auto" />}
                 </button>
             </div>
             <nav className={`transition w-full md:flex md:flex-row md:w-auto ${isMenuOpen ? "visible" : "invisible md:visible md:h-auto h-0"}`}>
                 <NavButton LinkTo="/blog">blog</NavButton >
                 <NavButton LinkTo="/showcase">showcase</NavButton>
                 <NavButton LinkTo="/service">lwdService</NavButton>
-                <NavButton LinkTo="/about">about me</NavButton >
+                <NavButton LinkTo="/about">about me</NavButton>
+                <LanguageSwitcher />
             </nav>
         </header>
     )
