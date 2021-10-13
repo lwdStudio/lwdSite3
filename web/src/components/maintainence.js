3f313dfe5ef039e59200a3c34a70c0ff599324de
Liwen Duan <hi@liwenduan.com>
2021-10-13 16:29:07 +0800

    code cleanse

--- web/src/components/maintainence.js
+++ web/src/components/maintainence.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Helmet } from 'react-helmet'
 import { StaticImage } from 'gatsby-plugin-image'
 import { StaticQuery,graphql } from 'gatsby'
+import { ExternalLinkIcon } from '@heroicons/react/outline'
 import Button from '../components/button'
 
 const Maintainence = () => {
@@ -49,18 +50,17 @@ const Maintainence = () => {
                         <div className="m-10">
                             <Button to="https://github.com/lwdStudio/lwdSite3">GitHub</Button>
                         </div>
-                        
-                            <footer>                  
-                                <div className="flex flex-col md:flex-row w-full justify-center text-center text-white">
-                                    <p className="m-3 p-1">Copyright © {new Date().getFullYear()} Liwen Duan</p>
-                                    <a href="http://beian.miit.gov.cn/" className="m-3 p-1 text-white">{data.site.siteMetadata.ICPlicense}</a>
-                                    <a href={data.site.siteMetadata.PSBeianURL} className="m-3 p-1 text-white">{data.site.siteMetadata.PSBeian}</a>
-                                </div>
-                            </footer>
+                        <footer>                  
+                            <div className="flex flex-col md:flex-row w-full justify-center text-center text-white">
+                                <p className="m-3 p-1">Copyright © {new Date().getFullYear()} Liwen Duan</p>
+                                <a href="http://beian.miit.gov.cn/" target="_blank" rel="noreferrer" className="m-3 p-1 text-white">{data.site.siteMetadata.ICPlicense}<ExternalLinkIcon className="h-3 inline px-1 align-middle"/></a>
+                                <a href={data.site.siteMetadata.PSBeianURL} target="_blank" rel="noreferrer" className="m-3 p-1 text-white">{data.site.siteMetadata.PSBeian}<ExternalLinkIcon className="h-3 inline px-1 align-middle"/></a>
+                            </div>
+                        </footer>
                     </div>
                 </div>
-                    }
-                />
+            }
+        />
 
     )
 }
