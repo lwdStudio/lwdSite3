f69248c52430168fe5ecdb9027490fa30356b885
Liwen Duan <hi@liwenduan.com>
2021-11-05 17:31:09 +0800

    new footer

--- web/src/components/page/footer.js
+++ web/src/components/page/footer.js
@@ -3,17 +3,20 @@ import { LwdLink } from './link'
 import { StaticImage } from 'gatsby-plugin-image'
 import { StaticQuery,graphql } from 'gatsby'
 
+const footerTextColor = 'text-gray-500 dark:text-gray-300'
+const footerLinkStyle = `${footerTextColor} p-1.5 rounded-md`
+
 export const FooterText = ({children}) => {
     return (
         <div className="py-1 px-4 lg:py-0">
-            <p className="flex text-gray-500 dark:text-gray-300 justify-center lg:justify-start m-0">{children}</p>
+            <p className={`flex ${footerTextColor} justify-center lg:justify-start m-0`}>{children}</p>
         </div>
         
     )
 }
 export const FooterLink = ({LinkTo, children}) => {
     return (
-        <FooterText><LwdLink LinkTo={LinkTo} className="text-gray-500 dark:text-gray-300 px-1.5 rounded-md" >{children}</LwdLink></FooterText>
+        <FooterText><LwdLink LinkTo={LinkTo} className={`${footerTextColor} px-1.5 rounded-md`} >{children}</LwdLink></FooterText>
     )
 }
 
@@ -33,8 +36,58 @@ const Footer = () => {
                 }
             `}
             render={data => 
-                <footer className="p-1 bg-gray-100 shadow-sm rounded-lg z-50 dark:bg-gray-800">
-                    <FooterLink LinkTo="/">
+                <footer className="p-1 bg-gray-100 shadow-sm rounded-lg z-50 dark:bg-gray-800 dark:text-white text-center md:text-left">
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-x-4 mx-8 place-items-stretch">
+                        <section className="pt-2">
+                            <FooterLink LinkTo="/">
+                                <div className="flex space-x-2 h-auto px-8 py-2 justify-center">
+                                    <div className="h-8 w-8 object-cover">
+                                        <StaticImage 
+                                            src="../../images/lwd-stamp.png"
+                                            alt="Liwen Duan's Logo"
+                                            placeholder="blurred"
+                                        />
+                                    </div>
+                                    <span className="inline-flex py-1 text-lg font-serif text-black dark:text-gray-200      dark:text-opacity-80">{data.site.siteMetadata.title}</span>
+                                </div>
+                            </FooterLink>
+                            <div className="my-2 md:ml-12">
+                                <ul className="list-none m-0 p-0">
+                                    <li><LwdLink className={footerLinkStyle} LinkTo="/tos">Legal Terms</LwdLink></li>
+                                    <li><LwdLink className={footerLinkStyle} LinkTo="https://status.liwenduan.com">System Status</LwdLink></li>
+                                </ul>
+                            </div>
+
+                        </section>
+                        <section className="pt-2">
+                            <h1 className={`${footerTextColor} font-extrabold text-lg md:text-2xl`}>Get in touch?</h1>
+                            <p className='p-0'>Send an email to: <br /><LwdLink className={footerLinkStyle} LinkTo="mailto:hi@liwenduan.com">hi@liwenduan.com</LwdLink></p>
+                            <p className='p-0'>Connect with me on: <br /><LwdLink className={footerLinkStyle} LinkTo="https://www.linkedin.com/in/lwd">LinkedIn</LwdLink></p>
+                        </section>
+                        {/* <section className="pt-2">
+                            <h1 className={`${footerTextColor} font-extrabold text-lg md:text-2xl`}>Set your experience:</h1>
+                            <ul className="list-none m-0 mt-1 p-0">
+                                <li>Dark mode: <LwdLink className={footerLinkStyle} LinkTo="/">on</LwdLink></li>
+                                <li>Language: <LwdLink className={footerLinkStyle} LinkTo="/">English</LwdLink></li>
+                            </ul>
+                        </section> */}
+                        <section className="pt-2">
+                            <h1 className={`${footerTextColor} font-extrabold text-lg md:text-2xl`}>Explore</h1>
+                            <ul className="list-none m-0 mt-1 p-0">
+                                <li><LwdLink className={footerLinkStyle} LinkTo="/blog">Blog</LwdLink></li>
+                                <li><LwdLink className={footerLinkStyle} LinkTo="/showcase">Showcase</LwdLink></li>
+                                <li><LwdLink className={footerLinkStyle} LinkTo="/lwdService">lwdService</LwdLink></li>
+                                <li><LwdLink className={footerLinkStyle} LinkTo="/about">About me</LwdLink></li>
+                            </ul>
+                        </section>
+                    </div>
+                    <hr className='m-8 mb-4 opacity-80' />
+                    <div className="grid grid-flow-row auto-rows-max px-8 pb-4 text-xs lg:grid-flow-col lg:auto-cols-max">
+                        <FooterText>Copyright © {new Date().getFullYear()} Liwen Duan</FooterText>
+                        <FooterLink LinkTo="http://beian.miit.gov.cn/">{data.site.siteMetadata.ICPlicense}</FooterLink>
+                        <FooterLink LinkTo={data.site.siteMetadata.PSBeianURL}>{data.site.siteMetadata.PSBeian}</FooterLink>                      
+                    </div>
+                    {/* <FooterLink LinkTo="/">
                         <div className="flex space-x-2 h-auto px-8 py-2 justify-center lg:justify-start">
                             <div className="h-8 w-8 object-cover">
                                 <StaticImage 
@@ -47,13 +100,7 @@ const Footer = () => {
                         </div>
                     </FooterLink>
                     
-                    <div className="grid grid-flow-row auto-rows-max py-2 px-8 text-xs lg:grid-flow-col lg:auto-cols-max">
-                        <FooterText>Copyright © {new Date().getFullYear()} Liwen Duan</FooterText>
-                        <FooterLink LinkTo="/tos">Legal Terms</FooterLink>
-                        <FooterLink LinkTo="https://status.liwenduan.com">System Status</FooterLink>
-                        <FooterLink LinkTo="http://beian.miit.gov.cn/">{data.site.siteMetadata.ICPlicense}</FooterLink>
-                        <FooterLink LinkTo={data.site.siteMetadata.PSBeianURL}>{data.site.siteMetadata.PSBeian}</FooterLink>                      
-                    </div>
+                     */}
                 </footer>
             }
         />
