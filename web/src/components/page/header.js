58302cf3cd38b59465c0a8af42ea06fb45cdfce2
Liwen Duan <hi@liwenduan.com>
2021-11-21 12:11:14 +0800

    Change menu bar icon to FontAwesome, remove heroicons

--- web/src/components/page/header.js
+++ web/src/components/page/header.js
@@ -1,28 +1,13 @@
 import React, {useState} from 'react'
 import { Link } from 'gatsby'
 import { StaticImage } from 'gatsby-plugin-image'
-import {GlobeAltIcon, MenuIcon, XIcon, SunIcon, MoonIcon } from '@heroicons/react/solid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
 
 
 const NavButton = ({LinkTo, children}) => {
     return (
         <Link to={LinkTo} className="flex w-full md:w-28 p-3 my-3 md:m-3 justify-center rounded-md md:shadow-md font-medium text-gray-700 focus:shadow-none dark:text-white dark:text-opacity-70 md:dark:bg-gray-700 md:dark:hover:bg-gray-600 md:dark:hover:text-white md:dark:focus:bg-yellow-300">{children}</Link>
-    )
-}
-
-const LanguageSwitcher = () => {
-    return (
-        <GlobeAltIcon className="flex h-12 w-full md:w-auto p-3 my-3 md:m-3 justify-center rounded-full text-gray-700 text-center align-center"/>
-    )
-}
-
-const DarkModeSwitch = (onClick) => {
-    return (
-        <div>
-            <button onClick={onClick} className="link-common rounded-full">
-                <MoonIcon className="flex h-12 w-full md:w-auto p-3 my-3 md:m-3 justify-center rounded-full text-gray-700 dark:text-gray-400 text-center align-center"/>
-            </button>
-        </div>
     )
 }
 
@@ -42,14 +27,14 @@ const LwdLogo = ({pageName}) => {
 
 const Header = ({pageName='lwdSite'}) => {
     const [isMenuOpen, setMenuOpen] = useState(false)
-    const MenuIconStyle = "h-8 w-auto"
+    const MenuIconStyle = "scale-150"
     return (
         <header className="p-1 flex flex-wrap md:flex rounded-b-lg shadow-lg bg-white dark:bg-gray-800 dark:text-white dark:text-opacity-50">
             <LwdLogo pageName={pageName} />
             <div className="flex-grow"></div>
             <div className="flex items-center md:hidden p-2 ">
-                <button onClick={() => setMenuOpen(!isMenuOpen)} className={`p-3 rounded-lg shadow-md dark:bg-gray-700`} aria-label="Menu">
-                    {isMenuOpen ? <XIcon className={MenuIconStyle} /> : <MenuIcon className={MenuIconStyle} />}
+                <button onClick={() => setMenuOpen(!isMenuOpen)} className={`h-12 w-12 rounded-lg shadow-md dark:bg-gray-700`} aria-label="Menu">
+                    {isMenuOpen ? <FontAwesomeIcon icon={faTimes} className={MenuIconStyle} /> : <FontAwesomeIcon icon={faBars} className={MenuIconStyle}/>}
                 </button>
             </div>
             <nav className={`transition w-full md:flex md:flex-row md:w-auto ${isMenuOpen ? "visible" : "invisible md:visible md:h-auto h-0"}`}>
