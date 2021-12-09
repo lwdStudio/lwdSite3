eeff69717048de0b8b59b718794dcb98fd82168f
Liwen Duan <hi@liwenduan.com>
2021-12-09 11:39:12 +0800

    LwdLink now has more icon

--- web/src/components/page/link.js
+++ web/src/components/page/link.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons'
+import { faExternalLinkAlt, faEnvelope, faPhoneAlt } from '@fortawesome/free-solid-svg-icons'
 
 export const LwdLink = ({LinkTo, className, children, ...props}) => {
-    if (LinkTo.startsWith('/')) {
+    if (LinkTo.startsWith("/")||LinkTo.startsWith("../")||LinkTo.startsWith("#")) {
         return (
             <Link to={LinkTo} className={`${className}` } {...props}>{children}</Link>
         )
-    }
-    else if (LinkTo.startsWith('../')) {
+    } else if (LinkTo.startsWith('mailto:')) {
         return (
-            <Link to={LinkTo} className={`${className}`} {...props}>{children}</Link>
+            <a href={LinkTo} target="_blank" rel="noreferrer" className={`${className}`} {...props}><FontAwesomeIcon icon={faEnvelope} className="scale-75 self-center"/> {children}</a>
+        )
+    } else if (LinkTo.startsWith('tel:')) {
+        return (
+            <a href={LinkTo} target="_blank" rel="noreferrer" className={`${className}`} {...props}><FontAwesomeIcon icon={faPhoneAlt} className="scale-75 self-center"/> {children}</a>
         )
     }
     return (
