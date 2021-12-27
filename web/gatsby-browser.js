6c4ed722bee09d59852df98521a58553fe5f5f8e
Liwen Duan <hi@liwenduan.com>
2021-12-27 17:28:11 +0800

    Update button style

--- web/gatsby-browser.js
+++ web/gatsby-browser.js
@@ -4,4 +4,4 @@ import '@fontsource/noto-sans-sc/500.css';
 import '@fontsource/noto-sans-sc/900.css';
 import '@fontsource/noto-serif-sc/500.css';
 import '@fontsource/noto-serif-sc/900.css';
-import '@fontsource/noto-mono'
+import '@fontsource/noto-mono/400.css'
--- web/src/components/button.js
+++ web/src/components/button.js
@@ -4,7 +4,7 @@ import AniLink from "gatsby-plugin-transition-link/AniLink"
 
 export const Button = ({to, className, children}) => {
     return (
-        <LwdLink LinkTo={to} className={`text-white dark:text-white place-self-center w-48 p-3 my-3 text-center rounded-md shadow-md shadow-blue-600/50 hover:shadow-yellow-300 font-medium  bg-blue-600 hover:bg-yellow-300 hover:text-black transition focus:shadow-none ${className}`}>
+        <LwdLink LinkTo={to} className={`text-white dark:text-white place-self-center w-48 p-3 my-3 text-center rounded-md shadow-md shadow-blue-600/50 font-medium  bg-blue-600 hover:bg-yellow-300 hover:shadow-yellow-300 hover:text-black dark:hover:bg-yellow-300 dark:hover:shadow-yellow-300 dark:hover:text-black transition focus:shadow-none ${className}`}>
             {children}
         </LwdLink>
     )
@@ -12,7 +12,7 @@ export const Button = ({to, className, children}) => {
 
 export const ColorDripButton = ({to, className, children}) => {
     return (
-        <AniLink paintDrip hex="#fcd34d" to={to} className={`text-white dark:text-white place-self-center w-48 p-3 my-3 text-center rounded-md shadow-md shadow-blue-600/50 hover:shadow-yellow-300 font-medium bg-blue-600 hover:bg-yellow-300 hover:text-black transition focus:shadow-none ${className}`}>
+        <AniLink paintDrip hex="#fcd34d" to={to} className={`text-white dark:text-white place-self-center w-48 p-3 my-3 text-center rounded-md shadow-md shadow-blue-600/50 font-medium bg-blue-600 hover:bg-yellow-300 hover:shadow-yellow-300 hover:text-black dark:hover:bg-yellow-300 dark:hover:shadow-yellow-300 dark:hover:text-black transition focus:shadow-none ${className}`}>
             {children}
         </AniLink>
     )
@@ -20,7 +20,7 @@ export const ColorDripButton = ({to, className, children}) => {
 
 export const SkipNav = () => {
     return (
-        <LwdLink paintDrip hex="#fcd34d" LinkTo="#main-content" className="absolute transform -translate-y-[150%] focus:translate-y-0 left-52 w-full md:w-48 p-3 my-3 md:m-3 text-center rounded-md md:shadow-md font-medium text-gray-700 focus:shadow-none dark:text-white dark:text-opacity-70 md:dark:bg-gray-700 md:dark:hover:bg-gray-600 md:dark:hover:text-white md:dark:focus:bg-yellow-300" aria-label="skip navigataion" aria-hidden="false" role='button'>Skip Navigation</LwdLink>
+        <LwdLink paintDrip hex="#fcd34d" LinkTo="#main-content" className="absolute transform -translate-x-[150%] focus:translate-x-0 md:left-16 left-[4.5rem] w-[10rem] p-3 my-3 md:m-3 text-center rounded-md md:shadow-md font-medium text-gray-700 focus:shadow-none dark:text-white dark:text-opacity-70 md:dark:focus:bg-yellow-300" aria-label="skip navigataion" aria-hidden="false" role='button'>Skip Navigation</LwdLink>
     )
 }
 
