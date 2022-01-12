f16a3edf1753c432f3fe402ec90c842a073e2c6b
Liwen Duan <hi@liwenduan.com>
2022-01-12 15:41:48 +0800

    Update about page with Skill section

--- web/src/pages/about.js
+++ web/src/pages/about.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Layout from '../components/page/layout'
 import Seo from "../components/seo"
-import {InfoCard, ContactCard, CertificateCard} from '../components/card'
+import {InfoCard, ContactCard, CertificateCard, SkillCard} from '../components/card'
 import {Button} from '../components/button'
 import {graphql} from 'gatsby'
 import {GatsbyImage} from 'gatsby-plugin-image'
 
 const AboutPage = ({location, data}) => {
-  const {h1, Description, Job, Education, ContactInfo, avatar, Certificate, Resume} = data.strapiAboutMe
+  const {h1, Description, Job, Education, ContactInfo, avatar, Certificate, Resume, Skills} = data.strapiAboutMe
   return (
     <Layout location={location} pageType="About me">
       <Seo title="About me" description={Description} image={`${process.env.GATSBY_STRAPI_API_URL}${avatar.url}`} pathname={location.pathname}/>
@@ -60,10 +60,32 @@ const AboutPage = ({location, data}) => {
             })
           }
         </section>
+        
+        <section className="p-5">
+          <h2 className="text-center font-serif py-8">Skills</h2>
+          {
+            Skills.map((skillCatagories) => {
+              return (
+                <div id={skillCatagories.id} className="grid grid-cols-2 sm:grid-cols-3">
+                  <h3 className="col-span-2 sm:col-span-1 p-4">{skillCatagories.SkillCatagory}</h3>
+                  <div className="col-span-2 flex flex-wrap p-2">
+                  {
+                    skillCatagories.Skill.map((skill) => {
+                      return (
+                          <SkillCard skill={skill} />
+                          )
+                        })
+                      }
+                  </div>
+                </div>
+              )
+            })
+          }
+        </section>
 
         <section className="p-5">
           <h2 className="text-center font-serif py-8">Get in touch</h2>
-          <div className="flex flex-wrap justify-start">
+          <div className="flex flex-wrap sm:justify-start justify-center align-middle">
             {
               ContactInfo.map((info) => {
                 return (
@@ -117,6 +139,15 @@ query getAboutMe {
     }
     Resume {
       url
+    }
+    Skills {
+      SkillCatagory
+      id
+      Skill {
+        id
+        SkillName
+        fabIcon
+      }
     }
   }
 }
